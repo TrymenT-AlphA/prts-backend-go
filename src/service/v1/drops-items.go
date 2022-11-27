ba1aa8e3269a631e24b92766390f6ea3d54d0f29
ChongKai <1302393176@qq.com>
2022-11-27 13:21:10 +0800

    [Fix] drops/items drops/stages

--- src/service/v1/drops-items.go
+++ src/service/v1/drops-items.go
@@ -5,8 +5,11 @@ import "prts-backend/src/model"
 func DropsItems() []model.Item {
 	var result []model.Item
 	db.
-		Model(&model.Item{}).
-		Having("COUNT(Drops) >= 1").
-		Find(&result)
+		Raw(`SELECT prts_item.*
+			FROM prts_item
+			JOIN prts_drop
+			ON prts_item.ID = prts_drop.ItemID
+			GROUP BY ID`).
+		Scan(&result)
 	return result
 }
--- src/service/v1/drops-stages.go
+++ src/service/v1/drops-stages.go
@@ -5,8 +5,11 @@ import "prts-backend/src/model"
 func DropsStages() []model.Stage {
 	var result []model.Stage
 	db.
-		Model(&model.Stage{}).
-		Having("COUNT(Drops) >= 1").
-		Find(&result)
+		Raw(`SELECT prts_stage.*
+			FROM prts_stage
+			JOIN prts_drop
+			ON prts_stage.ID = prts_drop.StageID
+			GROUP BY ID`).
+		Scan(&result)
 	return result
 }
